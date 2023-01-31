8e42002a412fb273577ef6ddeb9b68159d021efd
Santiago Quiroga Turdera <sanqui.94@gmail.com>
2023-01-31 18:40:59 -0400

    Addded error handling and modified input to become a form

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,53 @@ function App() {
   const [url, setUrl] = useState(
     'https://hn.algolia.com/api/v1/search?query=redux',
   );
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+
   useEffect(() => {
       async function fetchData() {
-        const result = await axios(url);
-        console.log('running down a dream!');
-        setData(result.data);
+        setIsLoading(true);
+        setIsError(false);
+        try {
+          const result = await axios(url);
+          setData(result.data);
+        } catch (error) {
+          setIsError(true);
+        }
+
+        setIsLoading(false);
       }
       fetchData();
   }, [url]);
 
   return (
     <>
-      <input type="text" value={query} onChange={event => setQuery(event.target.value)} />
-      <button type="button" onClick={() => setUrl(`https://hn.algolia.com/api/v1/search?query=${query}`)}>
-        Search
-      </button>
-      <ul>
-        {data.hits.map(item => (
-          <li key={item.objectID}>
+      <form 
+        onSubmit={event => {
+          setUrl(`https://hn.algolia.com/api/v1/search?query=${query}`);
+          event.preventDefault();
+      }}>
+        <input type="text" value={query} onChange={event => setQuery(event.target.value)} />
+        <button type="submit">Search</button>
+      </form>
+
+      {isError && <div>Something went wrong</div>}
+      {isLoading ? (
+        <div>
+          Loading...
+        </div>
+      ) : (
+        <ul>
+          {data.hits.map(item => (
+            <li key={item.objectID}>
+              <a href={item.url}>{item.title}</a>    
             <a href={item.url}>{item.title}</a>    
-          </li>
-        ))}
-      </ul>
+              <a href={item.url}>{item.title}</a>    
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
