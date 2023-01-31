948b75049560151240da96dced0604ffb1e478cf
Santiago Quiroga Turdera <sanqui.94@gmail.com>
2023-01-31 12:41:05 -0400

    added button and query updates logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,33 @@ import axios from 'axios';
 
 function App() {
   const [data, setData] = useState({ hits: [] });
-
+  const [query, setQuery] = useState('redux');
+  const [url, setUrl] = useState(
+    'https://hn.algolia.com/api/v1/search?query=redux',
+  );
   useEffect(() => {
       async function fetchData() {
-        const result = await axios('https://hn.algolia.com/api/v1/search?query=redux');
+        const result = await axios(url);
+        console.log('running down a dream!');
         setData(result.data);
       }
       fetchData();
-  });
+  }, [url]);
 
   return (
-    <ul>
-      {data.hits.map(item => (
-        <li key={item.objectID}>
-          <a href={item.url}>{item.title}</a>    
-        </li>
-      ))}
-    </ul>
+    <>
+      <input type="text" value={query} onChange={event => setQuery(event.target.value)} />
+      <button type="button" onClick={() => setUrl(`https://hn.algolia.com/api/v1/search?query=${query}`)}>
+        Search
+      </button>
+      <ul>
+        {data.hits.map(item => (
+          <li key={item.objectID}>
+            <a href={item.url}>{item.title}</a>    
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
